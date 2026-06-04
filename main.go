3724535f6c5a679eb1ae9443beb4f1641c743162
ryxa <rayhansatriabudi@gmail.com>
2026-06-04 18:50:25 +0700

    feat: add pengurutan berdasarkan harga dan pencarian berdasarkan nama dan harga

--- main.go
+++ main.go
@@ -76,20 +76,86 @@ func tampilMenuNonKopi(arr tabKopi, N int) {
 	}
 }
 
+func urutkanHarga(arr *tabKopi, N int) {
+	var i, j int
+	var key katalogKopi
+	for i = 1; i < N; i++ {
+		key = (*arr)[i]
+		j = i - 1
+		for j >= 0 && (*arr)[j].harga > key.harga {
+			(*arr)[j+1] = (*arr)[j]
+			j--
+		}
+		(*arr)[j+1] = key
+	}
+}
+
+func cariNama(T tabKopi, N int, target string) {
+	var l, r, mid int
+	l = 0
+	r = N - 1
+	mid = (l + r) / 2
+
+	for l <= r {
+		if T[mid].namaMinuman == target {
+			fmt.Printf("Minuman ditemukan: %s - Rp%d\n", T[mid].namaMinuman, T[mid].harga)
+			return
+		} else if T[mid].namaMinuman < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+		mid = (l + r) / 2
+	}
+	fmt.Println("Minuman tidak ditemukan")
+}
+
+func cariHarga(T tabKopi, N int, t int) {
+	var i int
+	for i = 0; i < N; i++ {
+		if T[i].harga == t {
+			fmt.Printf("%s - Rp%d\n", T[i].namaMinuman, T[i].harga)
+		}
+	}
+}
+
 func main() {
-	var n, pilih int
+	var n, pilih, fitur int
 	var jumlahKopi, jumlahNonkopi int
 	var arr tabKopi
-	fmt.Print("Menu cafe kami:")
-	fmt.Print("\n1. Kopi\n2. Non-Kopi\nPilih menu yang ingin anda lihat:\n")
-	fmt.Scanln(&pilih)
-	if pilih == 1 {
+	fmt.Print("fitur: 1.Beli menu\n2. cari berdasarkan nama \n3. cari berdasarkan harga\n4. urutkan harga termurah\n5. Update Menu\n6.Hapus Menu\n Pilih fitur yang ingin anda gunakan:(1-6)\n")
+	if fitur == 1 {
+		fmt.Print("Menu cafe kami:")
+		fmt.Print("\n1. Kopi\n2. Non-Kopi\nPilih menu yang ingin anda lihat:\n")
+		fmt.Scanln(&pilih)
+		for true {
+			if pilih == 1 {
+				tampilMenuKopi(arr, jumlahKopi)
+				fmt.Print("Masukan jumlah kopi yang mau anda beli:\n")
+				inputKopi(arr, n)
+			} else if pilih == 2 {
+				tampilMenuNonKopi(arr, jumlahNonkopi)
+				fmt.Print("Masukan jumlah non-kopi yang mau anda beli:\n")
+				inputKopi(arr, n)
+			} else {
+				break
+			}
+		}
+	} else if fitur == 2 {
+		var target string
+		fmt.Print("Masukan nama minuman yang ingin anda cari:")
+		fmt.Scan(&target)
+		cariNama(arr, n, target)
+	} else if fitur == 3 {
+		var targetHarga int
+		fmt.Scan(&targetHarga)
+		cariHarga(arr, n, targetHarga)
+		fmt.Print("Masukan harga minuman yang ingin anda cari:")
+
+	} else if fitur == 4 {
+		urutkanHarga(&arr, n)
+		fmt.Print("Menu minuman setelah diurutkan berdasarkan harga termurah:\n")
 		tampilMenuKopi(arr, jumlahKopi)
-		fmt.Print("Masukan jumlah kopi yang mau anda beli:\n")
-		inputKopi(arr, n)
-	} else {
 		tampilMenuNonKopi(arr, jumlahNonkopi)
-		fmt.Print("Masukan jumlah non-kopi yang mau anda beli:\n")
-		inputKopi(arr, n)
 	}
 }
